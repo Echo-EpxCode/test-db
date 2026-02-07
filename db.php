8545919c1245e3155c526c79aa276627e0820186
Jonefer <codillojonefer45@gmail.com>
2026-02-07 19:07:02 +0800

    Update database process-Auto Create db

--- db.php
+++ db.php
@@ -1,17 +1,24 @@
 <?php
+$conn = mysqli_connect("localhost", "root", "");
 
-// Database connection ni Juday
-$host = "localhost";
-$user = "root";
-$pwd = "";
-$db = "test_db";
+if (!$conn) {
+    die("Connection failed");
+}
 
-$conn = mysqli_connect($host,$user,$pwd,$db);
-  if(mysqli_connect_error()) {
-    die ("Failed to connect" .mysqli_connect_error());
-  }
+$sql = "CREATE DATABASE IF NOT EXISTS test_db";
+mysqli_query($conn, $sql);
 
+mysqli_select_db($conn, "test_db");
 
+$table = "CREATE TABLE IF NOT EXISTS `user_tbl` (
+  `id` int(11) NOT NULL,
+  `Firstname` varchar(50) NOT NULL,
+  `Lastname` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+)";
 
+mysqli_query($conn, $table);
 
 
